cd2924640a8178037f8b6a73cdd973d44583d74e
siyabhatkal2010 <siyabhatkal2010@tamu.edu>
2025-12-07 15:34:03 -0600

    new changes

--- server/index.js
+++ server/index.js
@@ -33,6 +33,14 @@ app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
 });
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
